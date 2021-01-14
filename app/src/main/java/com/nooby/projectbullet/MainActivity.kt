65648a13a9813475405618665d0541886911a986
Sam <sam.newby@mail.utorontu.ca>
2021-01-14 18:15:11 -0500

    Trying themes

--- app/src/main/java/com/nooby/projectbullet/MainActivity.kt
+++ app/src/main/java/com/nooby/projectbullet/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.nooby.projectbullet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +22,15 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         Log.i("MainActivity", "MainActivity created")
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        //Sets up the top toolbar and the navigation
+        val navController = findNavController(R.id.host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onDestroy() {
