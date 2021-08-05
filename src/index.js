670f88f34f6214b523204bd297fb9997b7b3f7fc
Sam <sam.newby@mail.utoronto.ca>
2021-08-05 07:23:51 -0400

    Add vuetify to project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import store from './store.js'
+import vuetify from './plugins/vuetify'
 
 //Creates a new vue object and mounts the App object to the #app div
 new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App)
 }).$mount('#app')
--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,30 @@ module.exports = {
                 loader: 'vue-loader'
             },
             {
-                test: /\.scss$/,
+                test: /\.s(c|a)ss$/,
                 use: [
                     'vue-style-loader',
                     'css-loader',
-                    'sass-loader'
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^7.0.0
+                        options: {
+                            implementation: require('sass'),
+                            indentedSyntax: true // optional
+                        },
+                        // Requires >= sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                indentedSyntax: true // optional
+                            },
+                        },
+                    },
                 ]
+            },
+            {
+                test: /\.css$/i,
+                use: ['vue-style-loader', 'css-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
