e91c05a664cbdcd79a699bc092b3c30a468192c5
Sam <sam.newby@mail.utoronto.ca>
2021-08-05 12:44:35 -0400

    Fixes SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -19,21 +19,7 @@ module.exports = {
                 use: [
                     'vue-style-loader',
                     'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        // Requires sass-loader@^7.0.0
-                        options: {
-                            implementation: require('sass'),
-                            indentedSyntax: true // optional
-                        },
-                        // Requires >= sass-loader@^8.0.0
-                        options: {
-                            implementation: require('sass'),
-                            sassOptions: {
-                                indentedSyntax: true // optional
-                            },
-                        },
-                    },
+                    'sass-loader'
                 ]
             },
             {
